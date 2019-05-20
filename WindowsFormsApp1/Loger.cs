d693f64a9238d0d813b5db8d4d492bf90b51ffba
Novel <Novel@DESKTOP-QSGCUMI>
2019-05-20 16:16:01 +0300

    1

--- WindowsFormsApp1/Loger.cs
+++ WindowsFormsApp1/Loger.cs
@@ -7,7 +7,7 @@ namespace WindowsFormsApp1
     {
         public static void WriteLog(string type, string method, string time, string ip, string status, string info)
         {
-            File.AppendAllText("log.txt", $"|{type}|Method: {method}|Time: {time}|Reqvest ip {ip}|Status: {status}|{info}|\n");
+            File.AppendAllText("log.txt", $"|{type}|Method: {method}|Time: {time}|Reqvest ip {ip}|Status: {status}|{info}|{Environment.NewLine}");
             Console.WriteLine($"|{type}|Method: {method}|Time: {time}|Reqvest ip {ip}|Status: {status}|{info}|");
         }
     }
